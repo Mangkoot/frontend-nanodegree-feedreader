ffaa15edfaf90088974693a207729bd995df3354
Magnus Aune <aunern2@gmail.com>
2018-09-04 22:40:18 +0200

    finished the Jasmine

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -98,9 +98,27 @@ $(function() {
          });
     });
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         var feedOne, feedTwo;
+
+         beforeEach(function(done) {
+            loadFeed(0, function() {
+              feedOne = $('.feed').html();
+              loadFeed(1, function() {
+                done();
+              });
+          });
+        });
+         it('content changes when feed is loaded', function(done) {
+            feedTwo = $('.feed').html();
+            expect(feedOne).not.toBe(feedTwo);
+            done();
+         });
+    });
 }());
